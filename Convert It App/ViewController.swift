b875734cbd7a0ac47995e37d0a4902c8947a1126
Nick <nickryan@Nicks-MacBook-Air.local>
2017-02-20 11:58:48 -0500

    Initial Commit

---
+++ Convert It App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Convert It App
+//
+//  Created by Nick on 2/20/17.
+//  Copyright © 2017 Nick Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
